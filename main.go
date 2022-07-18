0307467a5e344b054146db68b4caebb9d812b679
Euan Meston <euan@automata.tech>
2022-07-18 15:25:16 +0100

    kinda usable CLI tool

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
+	"path"
+	"strings"
 )
 
 func main() {
-	const csvFp string = "381.csv"
-	PrintTopTotal(csvFp)
+	var lifterName = strings.Join(os.Args[1:], " ")
+	const csvDir string = "UK"
+	csvFiles, err := os.ReadDir(csvDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, csvPath := range csvFiles {
+		var result = GetLifterResult(path.Join(csvDir, csvPath.Name()), lifterName)
+		if len(result) != 0 {
+			fmt.Println(result)
+		}
+	}
 }
 
-func PrintTopTotal(resultFp string) {
+func GetLifterResult(resultFp string, lifter string) []string {
+	var lifterResult []string
 	file, err := os.Open(resultFp)
 	if err != nil {
 		fmt.Println(err)
@@ -19,6 +33,9 @@ func PrintTopTotal(resultFp string) {
 	csvReader := csv.NewReader(file)
 	results, _ := csvReader.ReadAll()
 	for _, line := range results {
-		fmt.Println(line[3], line[13])
+		if strings.ToLower(line[3]) == lifter {
+			lifterResult = line
+		}
 	}
+	return lifterResult
 }
